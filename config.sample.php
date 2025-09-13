49a0505d15d1ca8e1739c703e06af45d483b0464
hnndhr <hananadhira1475@gmail.com>
2025-09-13 14:25:32 +0700

    config sample
    copas config sample ke config.php

---
+++ config.sample.php
@@ -0,0 +1,6 @@
+<?php
+// Copy to config.php and fill values
+define('DB_HOST','127.0.0.1');
+define('DB_USER','user');
+define('DB_PASS','user123');
+// no default DBNAME because queries will call db_buku.* and db_peminjaman.*
